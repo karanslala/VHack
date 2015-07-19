633e378fb353a720d6b80bf8678f218a73605819
Guiseppie Pagliaro <Guiseppie.Pagliaro@viasat.com>
2015-07-19 01:02:54 -0700

    REDOWNLOADED W/ Change to xcode proj

--- Educate/ViewController.swift
+++ Educate/ViewController.swift
@@ -13,17 +13,9 @@ import Parse
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
+        
         super.viewDidLoad()
         sleep(1)
-       // let testObject = PFObject(className: "TestObject")
-        //testObject["foo"] = "bar"
-        //testObject.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
-         //   println("Object has been saved.")
-        //}
         // Do any additional setup after loading the view, typically from a nib.
     }
 
