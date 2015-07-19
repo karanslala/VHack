5d059e302e29876c55dc964b0b37c01cedb84c98
Karan Lala <karan.lala@viasat.com>
2015-07-18 21:02:33 -0700

    Added Subject Page

--- Educate/ViewController.swift
+++ Educate/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        sleep(2)
+        sleep(1)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
